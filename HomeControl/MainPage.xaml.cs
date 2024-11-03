302c474f58610d5a4e213d27c83e6e5b16828dd2
Rodrigo <rodrigocaiokell@gmail.com>
2024-11-03 16:48:48 -0300

    pagina incial de cadastro de itens(valor ao vivo incompleto)

--- HomeControl/MainPage.xaml.cs
+++ HomeControl/MainPage.xaml.cs
@@ -4,19 +4,30 @@ namespace HomeControl
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
         private void OnSalvarCliked(object sender, EventArgs e)
         {
-            model.Item item = new model.Item();
-            item.Categoria = DataEntry.Text;
-            ResultadoLabel.Text = item.Categoria.ToString();
+            model.Item item = new((int)Id.Value, Nome.Text, (int)Quantidade.Value, Validade.Text);
+            item.Id = (int)Id.Value;
+            item.Validade = Validade.Text;
+            item.Nome = Nome.Text;
+            item.Quantidade = (int)Quantidade.Value;
+            //item.Categoria = DataEntry.Text;
+            ResultadoLabel.Text = item.ToString();
 
         }
+
+        //private void UpdateCounter(object sender, ValueChangedEventArgs e)
+        //{
+        //    var teste = IdCounter.Text;
+        //     IdCounter.Text = $"Valor {e.NewValue.ToString()}";
+        //    //var Controler = sender as Stepper;
+        //    //var Counter =  // this.FindByName<Label>($"{Controler.AutomationId}Counter");
+        //    //Counter.Text = $"valor: {e.NewValue}";
+        //}
     }
 }
 
--- HomeControl/model/item.cs
+++ HomeControl/model/item.cs
@@ -1,19 +1,22 @@
 namespace HomeControl.model;
 public class Item
-    {
-    public int Id {get; set;}
-    public string Nome {get; set;}
-    public string Quantidade {get; set;}
-    public string Validade {get; set;}
-    public Categoria Categoria {get; set;}
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public string Validade { get; set; }
+    public Categoria? Categoria { get; set; }
 
-        public Item(int id, string nome, string quantidade, string validade, Categoria categoria)
-        {
-            Id = id;
-            Nome = nome;
-            Quantidade = quantidade;
-            Validade = validade;
-            Categoria = categoria;
-        }
+    public Item(int id, string nome, int quantidade, string validade) //Categoria categoria)
+    {
+        Id = id;
+        Nome = nome;
+        Quantidade = quantidade;
+        Validade = validade;
+        //Categoria = categoria;
+    }
+    public override string ToString()
+    {
+        return $"{Id},{Nome}, {Quantidade}, {Validade}";
     }
 }
